9bf7c406b96324c4da3849f82fcb3c1970a7a57e
Emil H. Clausen <sequoiia@hummel.yt>
2021-01-19 13:01:21 +0100

    Added sample test projects'

---
+++ CostJanitor.Application.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace CostJanitor.Application.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
