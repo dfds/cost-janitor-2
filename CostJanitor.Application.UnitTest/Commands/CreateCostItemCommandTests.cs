87ebc250cb15606dbba6bdcefc49daf3234b7f22
Emil H. Clausen <sequoiia@hummel.yt>
2021-02-01 11:23:24 +0100

    Added tests for CreateCostItemCommand and CreateCostItemCommandHandler

---
+++ CostJanitor.Application.UnitTest/Commands/CreateCostItemCommandTests.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Text.Json;
+using CostJanitor.Application.Commands;
+using Xunit;
+
+namespace CostJanitor.Application.UnitTest.Commands
+{
+    public class CreateCostItemCommandTests
+    {
+        [Fact]
+        public void CanBeConstructed()
+        {
+            //Arrange
+            var sut = new CreateCostItemCommand("a", "b", "c");
+            //Act
+            var hashCode = sut.GetHashCode();
+
+            //Assert
+            Assert.NotNull(sut);
+            Assert.Equal(hashCode, sut.GetHashCode());
+            Assert.True(!string.IsNullOrEmpty(sut.CapabilityIdentifier));
+            Assert.True(!string.IsNullOrEmpty(sut.Label));
+            Assert.True(!string.IsNullOrEmpty(sut.Value));
+        }
+
+        [Fact]
+        public void CanBeSerialized()
+        {
+            //Arrange
+            var sut = new CreateCostItemCommand("a", "b", "c");
+
+
+            //Act
+            var json = JsonSerializer.Serialize(sut);
+
+            //Assert
+            Assert.False(string.IsNullOrEmpty(json));
+        }
+
+        [Fact]
+        public void CanBeDeserialized()
+        {
+            //Arrange
+            CreateCostItemCommand sut;
+            var json = "{\"capabilityIdentifier\": \"a\", \"label\": \"b\", \"value\": \"c\"}";
+
+            //Act
+            sut = JsonSerializer.Deserialize<CreateCostItemCommand>(json);
+
+            //Assert
+            Assert.NotNull(sut);
+            Assert.Equal("a", sut.CapabilityIdentifier);
+            Assert.Equal("b", sut.Label);
+            Assert.Equal("c", sut.Value);
+        }
+    }
+}
