f1ada1573a51f321193d54eda850d8990ad97ec3
Emil H. Clausen <sequoiia@hummel.yt>
2021-02-05 13:45:52 +0100

    Added AwsCostClientTests

---
+++ CostJanitor.Infrastructure.IntegrationTest/CostProviders/Aws/AwsCostClientTests.cs
@@ -0,0 +1,24 @@
+using System.Net;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.CostExplorer;
+using CostJanitor.Infrastructure.CostProviders.Aws;
+using Xunit;
+
+namespace CostJanitor.Infrastructure.IntegrationTest.CostProviders.Aws
+{
+    // This assumes a valid AWS session with the right permissions (CostExplorer access)
+    public class AwsCostClientTests
+    {
+        [Fact]
+        public async Task GetMonthlyTotalCostAllAccountsTest()
+        {
+            IAwsCostClient sut = new AwsCostClient(new AmazonCostExplorerClient(RegionEndpoint.USEast1));
+            var resp = await sut.GetMonthlyTotalCostAllAccounts();
+            
+            Assert.Equal(HttpStatusCode.OK, resp.HttpStatusCode);
+            Assert.NotEmpty(resp.ResultsByTime);
+            Assert.NotEmpty(resp.DimensionValueAttributes);
+        }
+    }
+}
--- CostJanitor.Infrastructure/CostProviders/Aws/IAwsCostClient.cs
+++ CostJanitor.Infrastructure/CostProviders/Aws/IAwsCostClient.cs
@@ -1,11 +1,12 @@
 using System.Threading.Tasks;
+using Amazon.CostExplorer.Model;
 using CostJanitor.Infrastructure.CostProviders.Aws.Model;
 
 namespace CostJanitor.Infrastructure.CostProviders.Aws
 {
     public interface IAwsCostClient : ICostProvider
     {
-        public Task<AwsCostResponse> GetMonthlyTotalCostAllAccounts();
-        public Task<AwsCostResponse> GetMonthlyTotalCostByAccountId(string accountId);
+        public Task<GetCostAndUsageResponse> GetMonthlyTotalCostAllAccounts();
+        public Task<GetCostAndUsageResponse> GetMonthlyTotalCostByAccountId(string accountId);
     }
 }
