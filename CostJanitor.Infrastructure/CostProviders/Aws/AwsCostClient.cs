134704f461cd0e0cc8a1bc999ca9fa428e714e60
Emil H. Clausen <sequoiia@hummel.yt>
2021-02-05 13:45:52 +0100

    Added AwsCostClientTests

--- CostJanitor.Infrastructure/CostProviders/Aws/AwsCostClient.cs
+++ CostJanitor.Infrastructure/CostProviders/Aws/AwsCostClient.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Amazon.CostExplorer;
+using Amazon.CostExplorer.Model;
 using CostJanitor.Infrastructure.CostProviders.Aws.Model;
 
 namespace CostJanitor.Infrastructure.CostProviders.Aws
@@ -13,12 +16,37 @@ namespace CostJanitor.Infrastructure.CostProviders.Aws
             _amazonCostExplorer = costExplorerClient;
         }
         
-        public async Task<AwsCostResponse> GetMonthlyTotalCostAllAccounts()
+        public async Task<GetCostAndUsageResponse> GetMonthlyTotalCostAllAccounts()
         {
-            throw new System.NotImplementedException();
+            var currentDate = DateTime.Now;
+            var firstDayOfMonth = new DateTime(currentDate.Year, currentDate.Month, 1);
+            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+            
+            var dateInterval = new DateInterval()
+            {
+                Start = $"{firstDayOfMonth.Year}-{firstDayOfMonth.Month.ToString("#00")}-{firstDayOfMonth.Day.ToString("#00")}",
+                End = $"{lastDayOfMonth.Year}-{lastDayOfMonth.Month.ToString("#00")}-{lastDayOfMonth.Day.ToString("#00")}"
+            };
+            
+            
+            var resp = await _amazonCostExplorer.GetCostAndUsageAsync(new GetCostAndUsageRequest()
+            {
+                Metrics = new List<string>(new []{Metric.BLENDED_COST.Value}),
+                TimePeriod = dateInterval,
+                Granularity = Granularity.MONTHLY,
+                GroupBy = new List<GroupDefinition>(new []
+                {
+                    new GroupDefinition()
+                    {
+                        Type = GroupDefinitionType.DIMENSION,
+                        Key = Dimension.LINKED_ACCOUNT.Value
+                    }
+                })
+            });
+            return resp;
         }
 
-        public async Task<AwsCostResponse> GetMonthlyTotalCostByAccountId(string accountId)
+        public async Task<GetCostAndUsageResponse> GetMonthlyTotalCostByAccountId(string accountId)
         {
             throw new System.NotImplementedException();
         }
