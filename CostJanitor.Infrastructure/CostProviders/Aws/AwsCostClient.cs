f03b6e04e55bc187729ff4dd381412641a694374
Emil H. Clausen <sequoiia@hummel.yt>
2021-02-05 11:07:13 +0100

    Added AWSSDK.Core and AWSSDK.CostExplorer libraries

--- CostJanitor.Infrastructure/CostProviders/Aws/AwsCostClient.cs
+++ CostJanitor.Infrastructure/CostProviders/Aws/AwsCostClient.cs
@@ -1,10 +1,18 @@
 using System.Threading.Tasks;
+using Amazon.CostExplorer;
 using CostJanitor.Infrastructure.CostProviders.Aws.Model;
 
 namespace CostJanitor.Infrastructure.CostProviders.Aws
 {
     public class AwsCostClient : IAwsCostClient
     {
+        private IAmazonCostExplorer _amazonCostExplorer;
+
+        public AwsCostClient(IAmazonCostExplorer costExplorerClient)
+        {
+            _amazonCostExplorer = costExplorerClient;
+        }
+        
         public async Task<AwsCostResponse> GetMonthlyTotalCostAllAccounts()
         {
             throw new System.NotImplementedException();
