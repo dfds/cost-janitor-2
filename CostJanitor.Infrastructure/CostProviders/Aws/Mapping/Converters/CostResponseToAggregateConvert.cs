f3c88ceb99d5da771f8eb4a8b6c899bd356d3b3d
Emil H. Clausen <sequoiia@hummel.yt>
2021-02-09 11:12:36 +0100

    WIP: Added some kind of logic to CostResponseToAggregateConvert, doesn't seem right

--- CostJanitor.Infrastructure/CostProviders/Aws/Mapping/Converters/CostResponseToAggregateConvert.cs
+++ CostJanitor.Infrastructure/CostProviders/Aws/Mapping/Converters/CostResponseToAggregateConvert.cs
@@ -3,6 +3,8 @@ using AutoMapper;
 using CostJanitor.Domain.Aggregates;
 using ResourceProvisioning.Abstractions.Aggregates;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CostJanitor.Infrastructure.CostProviders.Aws.Mapping.Converters
 {
@@ -15,7 +17,30 @@ namespace CostJanitor.Infrastructure.CostProviders.Aws.Mapping.Converters
                 case GetCostAndUsageResponse dto:
                     var reportAggr = new ReportItem(Guid.NewGuid());
 
-                    
+                    var accountResults = new Dictionary<string, string>();
+                    foreach (var dimensionValueAttribute in dto.DimensionValueAttributes)
+                    {
+                        var awsAccountName = dimensionValueAttribute.Attributes["description"];
+                        var awsAccountId = dimensionValueAttribute.Value;
+                        accountResults.Add(awsAccountId, awsAccountName);
+                    }
+
+                    foreach (var resultByTime in dto.ResultsByTime)
+                    {
+                        var awsAccountName = accountResults[resultByTime.Groups.First().Keys.First()];
+                        
+                        //TODO: Decide whether or not to do magic here
+                        var assumedCapabilityIdentifier = "";
+                        if (awsAccountName.Contains("dfds-"))
+                        {
+                            assumedCapabilityIdentifier = awsAccountName.Remove(0, 5);
+                        }
+                        // Magic end
+                        
+                        new CostItem("monthlyTotalCost", resultByTime.Groups.First().Metrics["BlendedCost"].Amount, assumedCapabilityIdentifier);
+                        
+                        reportAggr.AddCostItem(assumedCapabilityIdentifier);
+                    }
 
                     return reportAggr;
 
