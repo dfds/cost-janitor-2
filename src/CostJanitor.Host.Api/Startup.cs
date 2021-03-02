f7c28d5b63a0ddbd38c9d2d27a13f22c6b00362a
Tobias Andersen <toban@dfds.com>
2021-03-02 20:57:37 +0100

    - Cleaning up startup

--- src/CostJanitor.Host.Api/Startup.cs
+++ src/CostJanitor.Host.Api/Startup.cs
@@ -18,7 +18,6 @@ namespace CostJanitor.Host.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             AddHostServices(services);
@@ -26,9 +25,10 @@ namespace CostJanitor.Host.Api
             services.AddApplication(Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseHttpsRedirection();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
